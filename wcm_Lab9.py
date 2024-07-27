80e219b073bbda48dc1692d9550b4611a0492297
Cooper-mastin8 <cooper.mastin8@gmail.com>
2024-07-27 15:08:33 -0400

    Main Function

--- wcm_Lab9.py
+++ wcm_Lab9.py
@@ -1,5 +1,5 @@
 #Lab_9
-
+from ppp_decode import decode
 def encode(password):
     encode_password = ''
     for char in password:
@@ -8,3 +8,32 @@ def encode(password):
         char = str(char)
         encode_password += char
     return encode_password
+
+def menu():
+    print("Menu")
+    print("-------------")
+    print("1. Encode")
+    print("2. Decode")
+    print("3. Exit")
+    print()
+
+if __name__ == '__main__':
+    while True:
+
+        menu()
+        option = int(input("Enter your choice: "))
+
+        if option == 1:
+            password = input("Please enter your password to encode: ")
+            password = encode(password)
+
+            print("Your password has been encoded and stored!")
+
+        if option == 2:
+            print(f'The encoded password is: {password}, and the original password is {decode(password)}.')
+
+
+
+
+
+
